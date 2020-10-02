54ca31537cd809751ce52809463f397402f299b3
felipe gomes <felipinhogoma@gmail.com>
2020-10-02 01:46:28 -0300

    Criação do projeto backend

---
+++ backend/transport-api/src/test/java/com/github/fgsantana/transportapi/TransportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fgsantana.transportapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
