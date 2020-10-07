1ed5a2bffc89d526e2673c2f804325202ec8b9d5
felipe gomes <felipinhogoma@gmail.com>
2020-10-06 23:36:44 -0300

    Criação de funções para verificar e inserir/remover modais da transportadora de acordo com checkbox

--- frontend/transport-app/src/app/transport/transport-info.component.ts
+++ frontend/transport-app/src/app/transport/transport-info.component.ts
@@ -7,8 +7,36 @@ import { Transport } from "./transport"
 })
 export class TransportInfoComponent  {
 
-transport: Transport;
+
+transport:Transport =   {id: 1,
+email: "empresa@gmail.com",
+nome: "Empresa",
+empresa: "Transportadora",
+telefone:"8134537602",
+celular: "",
+whatsapp: "",
+modais: ['Aeroviario', 'Ferroviario', 'Dutoviario'],
+cep: "12345678",
+uf: "SP",
+cidade: "São Paulo",
+bairro: "Morumbi",
+logradouro: "Rua Teste",
+numero: 453,
+logoUrl: "http://localhost:8080/api/v1/transport/1/logo"
 
 
 }
-
+verify(modal: string) : boolean{
+    return this.transport.modais.indexOf(modal) > -1
+}
+change(modal: string) : void{
+    if(this.transport.modais.indexOf(modal) > -1){
+     this.transport.modais=this.transport.modais.filter(m => m!=modal);
+            console.log(this.transport.modais)
+    }
+    else{
+        this.transport.modais.push(modal);
+        console.log(this.transport.modais)
+    }
+}
+}
