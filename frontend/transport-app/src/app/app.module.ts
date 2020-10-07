85650203842fb4331ec6cab38b1894f5878d951d
felipe gomes <felipinhogoma@gmail.com>
2020-10-06 23:39:27 -0300

    Adição do módulo FormsModule para two-way databind no formulário

--- frontend/transport-app/src/app/app.module.ts
+++ frontend/transport-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { TransportInfoComponent } from './transport/transport-info.component'
+import { TransportInfoComponent } from './transport/transport-info.component';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     
   ],
   providers: [],
